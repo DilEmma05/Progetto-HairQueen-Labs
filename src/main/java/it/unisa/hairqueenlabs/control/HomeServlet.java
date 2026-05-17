66c40034b38d390fe248f0b6559e22de8589f0c9
emma <emma.bove5379@gmail.com>
2026-05-17 20:06:35 +0200

    Creazione servlet HomeServlet

---
+++ src/main/java/it/unisa/hairqueenlabs/control/HomeServlet.java
@@ -0,0 +1,58 @@
+package it.unisa.hairqueenlabs.control;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+import it.unisa.hairqueenlabs.dao.ProdottoDAO;
+import it.unisa.hairqueenlabs.model.Prodotto;
+
+/**
+ * Servlet implementation class HomeServlet
+ */
+@WebServlet("/HomeServlet")
+public class HomeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HomeServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		ProdottoDAO prodottoDAO = new ProdottoDAO();
+		
+		try {
+			//Recupero prodotti dal database
+			List<Prodotto> catalogo = prodottoDAO.doRetrieveAll();
+			request.setAttribute("listaProdotti", catalogo);
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/index.jsp");
+			dispatcher.forward(request, response);
+		}catch(SQLException e) {
+			System.out.println("Errore nel recupero dei prodotti dalla HomeServlet");
+			e.printStackTrace();
+			response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Errore nel database");
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
