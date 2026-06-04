cee2d5413e9676d7a239066be05478999836ec63
emma <emma.bove5379@gmail.com>
2026-06-04 20:18:36 +0200

    Aggiornato percorso JSP in WEB-INF

--- src/main/java/it/unisa/hairqueenlabs/control/CatalogoServlet.java
+++ src/main/java/it/unisa/hairqueenlabs/control/CatalogoServlet.java
@@ -44,7 +44,7 @@ public class CatalogoServlet extends HttpServlet {
 			List<Categoria> categorie = categoriaDAO.doRetrieveAllCategorie(); 
 			request.setAttribute("listaCategorie", categorie);
 			
-			RequestDispatcher dispatcher = request.getRequestDispatcher("/catalogo.jsp");
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/view/catalogo.jsp");
 			dispatcher.forward(request, response);
 			
 		} catch(SQLException e) {
